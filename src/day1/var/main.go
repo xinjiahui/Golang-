a280fc17e8d8d2a9fe9b35d5b82e6a352b1b182f
XinJiahui <xinjiahui@sensetimes-MacBook-Air.local>
2022-04-11 22:49:49 +0800

    add var

--- src/day1/var/main.go
+++ src/day1/var/main.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-//声明变量
+//全局声明变量
 var (
 	name string // ""
 	age int //0
@@ -13,11 +13,17 @@ func main(){
 	name = "理想"
 	age = 16
 	isOk = true
-	//Go语言中声明变量不必使用
+	//Go语言中非全局变量声明后必须使用
 	fmt.Print(isOk)
 	fmt.Println()
 	fmt.Println(age) //有换行   
 	fmt.Printf("name:%s",name) //%s 占位符
 	fmt.Println()
+	//声明变量同时赋值
+	var  s1 string = "whb"
+	fmt.Println(s1)
+	//简短变量声明,只能在函数中用
+	s3 := "hahaha"
+	fmt.Println(s3)
 
 }
