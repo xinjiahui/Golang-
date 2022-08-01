69d232de1bed4e852b4b6a0c0e11d84ad9ce3f2d
xinjiahui <xinjiahui@sensetime.com>
2022-08-01 15:10:18 +0800

    test

--- src/day19-0725/method_demo/main.go
+++ src/day19-0725/method_demo/main.go
@@ -3,7 +3,10 @@ package main
 import "fmt"
 
 //方法
+// 标识符：变量名 函数名 类型名 方法名
+// Go语言中如果标识符首字母是大写的，就表示对外部包可见（暴露的，公有的）
 
+// dog 这是一个狗的结构体
 type dog struct {
 	name string
 }
@@ -15,12 +18,32 @@ func newDog(name string) dog {
 	}
 }
 
+type persion struct {
+	name string
+	age  int
+}
+
+func newPersion(name string, age int) *persion {
+	return &persion{
+		name: name,
+		age:  age,
+	}
+}
+
 // 方法是作用于特定类型的函数
 // 接受者表示的是调用该方法的具体类型变量，多用类型名首字母小写字母表示
 func (d dog) wang() {
 	fmt.Printf("%s: 汪汪汪～", d.name)
 }
+func (p *persion) guonian() {
+	p.age++
+}
 func main() {
 	d1 := newDog("球球")
 	d1.wang()
+	p1 := newPersion("terst", 18)
+	fmt.Println(p1.age)
+	p1.guonian()
+	fmt.Println(p1.age)
+
 }
---
+++ src/day20-0801/method_demo1/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+//给自定义类型加方法
+type myInt int
+
+func (m myInt) hello() {
+	fmt.Println("我是一个int")
+}
+
+func main() {
+	m := myInt(100)
+	m.hello()
+}
